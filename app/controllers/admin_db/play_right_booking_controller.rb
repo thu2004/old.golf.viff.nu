a1438ae2cc8e7db3d2ead6dcbfa2b058ee5fce57
Thu <thu@thus-gb334aa-b1u-d4895se-2.local>
2010-03-31 21:33:00 +0200

    removed role and play_right in admin_db

--- app/controllers/admin_db/play_right_booking_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdminDb::PlayRightBookingController < Member::ApplicationController
-  
-	before_filter :verify_admin
-
-  active_scaffold :play_right_booking do |config|
-	    config.actions.exclude :show
-	    config.columns = [:play_right, :user, :num_of_resource, :when, :information]
-	  end
-end
