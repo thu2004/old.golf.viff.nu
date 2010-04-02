d5bdfa332e736bc93ffeb11495df5f09627a2bd2
Thu <thu@thus-gb334aa-b1u-d4895se-2.local>
2010-04-02 22:18:05 +0200

    disable Image plugin.activity

--- vendor/plugins/images/rails/init.rb
+++ vendor/plugins/images/rails/init.rb
@@ -3,12 +3,15 @@ Refinery::Plugin.register do |plugin|
   plugin.description = "Manage images"
   plugin.version = 1.0
   plugin.menu_match = /admin\/image(_dialog)?s$/
-  plugin.activity = {
-    :class => Image,
-    :title => 'title',
-    :url_prefix => 'edit',
-    :conditions => "parent_id IS NULL",
-    :created_image => "image_add.png",
-    :updated_image => "image_edit.png"
-  }
+  
+  # complained by Heroku when overwritten Image file
+  
+  # plugin.activity = {
+  #   :class => Image,
+  #   :title => 'title',
+  #   :url_prefix => 'edit',
+  #   :conditions => "parent_id IS NULL",
+  #   :created_image => "image_add.png",
+  #   :updated_image => "image_edit.png"
+  # }
 end
