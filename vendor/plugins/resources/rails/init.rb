cea7b9208a32ed3ef370b82b18c6e6b310edd246
Thu <thu@thus-gb334aa-b1u-d4895se-2.local>
2010-04-02 22:21:03 +0200

    disable Resource plugin.activity

--- vendor/plugins/resources/rails/init.rb
+++ vendor/plugins/resources/rails/init.rb
@@ -2,11 +2,14 @@ Refinery::Plugin.register do |plugin|
   plugin.title = "Resources"
   plugin.description = "Upload and link to files"
   plugin.version = 1.0
-  plugin.activity = {
-    :class => Resource,
-    :title => 'title',
-    :url_prefix => 'edit',
-    :created_image => "page_white_put.png",
-    :updated_image => "page_white_edit.png"
-  }
+  
+  # complained by Heroku when overwritten Resource file
+  
+  # plugin.activity = {
+  #     :class => Resource,
+  #     :title => 'title',
+  #     :url_prefix => 'edit',
+  #     :created_image => "page_white_put.png",
+  #     :updated_image => "page_white_edit.png"
+  #   }
 end
