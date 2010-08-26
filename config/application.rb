23f2e5ac8ea1f9f4ca4825205f5e35e508edeb0b
Thu2004 <thu2004@yahoo.se>
2010-08-26 22:32:58 +0200

    use rails v2.3.8

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Specified gem version of Refinery to use when vendor/plugins/refinery/lib/refinery.rb is not present.
 REFINERY_GEM_VERSION = '0.9.6.26' unless defined? REFINERY_GEM_VERSION
